3090b7706b6c38b249fd4b52d44f51ed15495de9
Raul <runnerdude08@gmail.com>
2018-02-17 20:55:15 -0800

    Added 'api' subdomaining and 'v1' to routes. Ex: api.lvh.me:3000/v1/...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :sources
+
+  # On localhost use api.lvh.me:3000
+  # lvh.me is alias for localhost!
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+
+        resources :sources do
+          resources :articles
+        end
+        resources :articles
+
+      end
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
