cb7b079b0def2ac26e2a982b35bdddb34aafbd3b
Raul <runnerdude08@gmail.com>
2018-02-19 17:11:19 -0800

    Added Job to purge articles older than 2 days from DB

---
+++ app/jobs/purge_old_articles_job.rb
@@ -0,0 +1,13 @@
+class PurgeOldArticlesJob < ApplicationJob
+  queue_as :default
+
+  def perform#(*args)
+    init_count = Article.count
+    Article.old_articles.each do |_article|
+      _article.destroy
+    end
+    finish_count = Article.count
+
+    puts "PURGE COMPLETE: Destroyed #{init_count - finish_count} old articles in DB"
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
     validates_presence_of :title, :url
     validates :url, format: { with: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/, message: 'must be in valid website format starting with either www or http(s)://' }
 
+    scope :old_articles, lambda { where('pub_date < ?', 2.days.ago) }
 end
