ffb0cf49b4c31b68fcd0913ada7ffa05ce4c5727
Raul <runnerdude08@gmail.com>
2018-02-20 13:20:49 -0800

    Added relations to serializer to expose source articles and article src

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,4 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :source_id, :title, :summary, :top_img, :url, :pub_date
+  belongs_to :source
 end
--- app/serializers/source_serializer.rb
+++ app/serializers/source_serializer.rb
@@ -1,3 +1,4 @@
 class SourceSerializer < ActiveModel::Serializer
   attributes :id, :name, :website
+  has_many :articles
 end
